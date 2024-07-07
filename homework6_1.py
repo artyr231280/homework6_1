6fe50f11982070321fa2699c37c9c29308e00ade
artyr231280 <artyr231280@mail.ru>
2024-07-07 19:05:20 +0500

    Add files via upload

---
+++ homework6_1.py
@@ -0,0 +1,10 @@
+my_set = {1,2,2,3,3,3,4,4,4,4,'a','b','c','d',True,False}
+print(my_set)
+list_ = [1,2,3,4]
+list_ = set(list_)
+print(list_)
+print(list_.add(5))
+print(list_.add(7))
+print(list_)
+print(list_.remove(4))
+print(list_)
